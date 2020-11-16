29c408d3dfeac42eb7cbeca470d026a3ace2a9c7
Arley Duarte <arley.duarte@zyght.com>
2020-11-16 17:49:43 +0000

    Add project files.

---
+++ TicTacToe/Game.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToe
+{
+    public class Game
+    {
+        public enum State
+        {
+            Cross, 
+            Zero,
+            Unset
+        }
+
+        public enum Winner
+        {
+            Zeroes,
+            Crosses,
+            Draw
+        }
+
+        
+
+        
+
+        public Game()
+        {
+            for (int i = 0; i < _board.Length; i++)
+            {
+                _board[i] = State.Unset;
+            }
+        }
+
+
+        private readonly State[] _board  = new State[9];
+        public double MovesCounter { get; private set; }
+
+        public Winner GetWinner()
+        {
+            return GetWinner(1, 4, 7, 2,5, 8, 3,6,9,4,5,6,7,8,9, 1,5,9,1,3,5);
+        }
+
+        public Winner GetWinner(params  int[] indexes)
+        {
+            for(int i= 0; i < indexes.Length; i+=3)
+            {
+                bool same = AreSame(indexes[i], indexes[i + 1], indexes[i + 2]);
+
+                if (same)
+                {
+                    var state = GetState(indexes[i]);
+
+                    if (state != State.Unset)
+                        return state == State.Cross ? Winner.Crosses : Winner.Zeroes;
+                }
+            }
+
+            return Winner.Draw;
+        }
+
+        private bool AreSame(int a, int b, int c)
+        {
+            return GetState(a) == GetState(b) && GetState(a) == GetState(c);
+        }
+
+        public void MakeMove(int index)
+        {
+            if(index < 1 || index > 9)
+                throw new ArgumentOutOfRangeException();
+
+
+            if(GetState(index) != State.Unset)
+                throw  new InvalidOperationException();
+
+            MovesCounter++;
+
+            
+            _board[index - 1] = MovesCounter % 2 == 0 ? State.Zero : State.Cross;
+        }
+
+        public State GetState(int index)
+        {
+            return _board[index - 1];
+        }
+    }
+}
---
+++ TicTacToeTests/GameTests.cs
@@ -0,0 +1,85 @@
+﻿using NUnit.Framework;
+using TicTacToe;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToe.Tests
+{
+    [TestFixture()]
+    public class GameTests
+    {
+
+
+        [Test()]
+        public void CreateGame_ZeroMoves()
+        {
+            var game = new Game();
+            Assert.AreEqual(0, game.MovesCounter);
+        }
+
+
+        [Test()]
+        public void MakeInvalidMove_ThrowsException()
+        {
+            var game = new Game();
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+            {
+                game.MakeMove(0);
+            });
+        }
+
+
+
+
+        [Test()]
+        public void MoveOnTheSameSquare_ThrowsException()
+        {
+            var game = new Game();
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                game.MakeMove(1);
+                game.MakeMove(1);
+            });
+        }
+
+        [Test()]
+        public void MakeMove_CounterShifts()
+        {
+            var game = new Game();
+            game.MakeMove(1);
+            game.MakeMove(2);
+            game.MakeMove(3);
+
+            Assert.AreEqual(Game.State.Cross, game.GetState(1));
+            Assert.AreEqual(Game.State.Zero, game.GetState(2));
+            Assert.AreEqual(Game.State.Cross, game.GetState(3));
+
+        }
+
+
+        [Test()]
+        public void GetWinner_ZeroesWin()
+        {
+            var game = new Game();
+
+            //2, 5, 8
+            MakeMoves(game, 1, 2, 3, 5, 4, 8 );
+
+          Assert.AreEqual(Game.Winner.Zeroes, game.GetWinner());
+        }
+
+
+
+        private void MakeMoves(Game game, params int[] indexes)
+        {
+            foreach (var index in indexes)
+            {
+                game.MakeMove(index);
+            }
+        }
+
+
+
+    }
+}
